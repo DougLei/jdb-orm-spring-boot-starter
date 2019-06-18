8961e672c833e4aeff2fe02e941164d2cff3f144
DougLei <953034206@qq.com>
2019-06-18 08:07:25 +0800

    first commit

---
+++ src/test/java/com/douglei/orm/spring/boot/starter/JdbOrmSpringBootStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.douglei.orm.spring.boot.starter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JdbOrmSpringBootStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
