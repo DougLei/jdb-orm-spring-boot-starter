0a2af82b3141208c89fe93e3be2bfeb6d24f02fd
DougLei <953034206@qq.com>
2019-08-13 18:13:58 +0800

    t

--- src/main/java/com/douglei/orm/spring/boot/starter/SessionFactoryRegisterHolderAutoConfiguration.java
+++ src/main/java/com/douglei/orm/spring/boot/starter/SessionFactoryRegisterHolderAutoConfiguration.java
@@ -1,7 +1,6 @@
 package com.douglei.orm.spring.boot.starter;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -16,14 +15,13 @@ import com.douglei.orm.spring.DestroyProxyBeanContextListener;
  * @author DougLei
  */
 @Configuration // 标明这是一个配置类
-@ConditionalOnClass(SessionFactoryRegister.class) // @ConditionalOnXXX, 满足指定条件时, 该配置类生效, 该配置表示当前classpath中存在SessionFactoryRegister类时, 该配置类生效
 @EnableConfigurationProperties(value = JdbOrmConfigurationProperties.class)
 public class SessionFactoryRegisterHolderAutoConfiguration {
+	private SessionFactoryRegister sessionFactoryRegister;
 	
 	@Autowired
 	private JdbOrmConfigurationProperties jdbOrmConfigurationProperties;
 	
-	private SessionFactoryRegister sessionFactoryRegister;
 	
 	@Bean // 将该方法产生的bean存储到spring容器中
 	@ConditionalOnMissingBean(SessionFactoryRegister.class) // 如果容器中不存在该类实例, 则创建该类的实例, 并加入到容器中
@@ -35,7 +33,7 @@ public class SessionFactoryRegisterHolderAutoConfiguration {
 	
 	// 注册默认的数据源
 	private void registerDefaultSessionFactory(ConfigurationWrapper defaultConfiguration) {
-		sessionFactoryRegister.registerDefaultSessionFactory(defaultConfiguration.getConfigurationFile(), defaultConfiguration.getDataSource(), defaultConfiguration.getMappingCacheStore(), false);
+		sessionFactoryRegister.registerDefaultSessionFactory(defaultConfiguration.getConfigurationFile(), defaultConfiguration.getDataSource(), defaultConfiguration.getMappingStore(), false);
 	}
 	
 	/**
